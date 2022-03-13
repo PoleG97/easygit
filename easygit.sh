4ae3b11d6d39d61addc8e3dc67b7a425f664a071
PoleG97 <44400218+PoleG97@users.noreply.github.com>
2022-03-13 14:03:24 +0100

    Colores

--- easygit.sh
+++ easygit.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 argumento=$1
 ruta="/mnt/c/Users/jairo/Desktop/Clase/Trabajos/BCI_lab"    # Cambiar por la ruta desada
 
 function arg_error {
-    echo "Debes proporcionar uno de estos argumentos"
+    echo -e "${yellowColour}[*]${endColour}Debes proporcionar uno de estos argumentos"
     echo "easygit.sh startlab"
     echo "easygit.sh endlab"
     echo "easygit.sh help"
@@ -28,12 +38,12 @@ if [ $# -eq 1 ] ; then
     elif [ $1 = "help" ] ; then                             # Ayuda sobre el uso de git
 
         echo -e "COMO TRABAJAR CON GIT \n"
-        echo "1) Ver los archivos cambiados (pendientes de subir): git status"
-        echo "2) Añadir esos archivos (todos) a la lista para subir: git add *"
+        echo "1) Ver los archivos cambiados (pendientes de subir): ${blueColour}git status${endColour}"
+        echo "2) Añadir esos archivos (todos) a la lista para subir: ${blueColour}git add *${endColour}"
         echo "   > Si quieres subir algunos determinado cambiar * por los nombres a subir"
-        echo "3) Añadir un commint antes de subir los cambios: git commit -m texto"
+        echo "3) Añadir un commint antes de subir los cambios: ${blueColour}git commit -m texto${endColour}"
         echo "   > Nota: el texto del commit debe ir entre comillas dobles"
-        echo "4) Con el commit hecho, hacemos el push: git push"
+        echo "4) Con el commit hecho, hacemos el push: ${blueColour}git push${endColour}"
         echo "   > Nota: si queremos el push a una rama concreta ponemos el nombre de la misma al final"
 
     else
